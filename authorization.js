33659f424453f19f45f90eade432bc0df207e06c
jgutix <whan.kharlos@gmail.com>
2020-05-11 01:35:46 -0600

    feat(middleware): add middleware for authorization

---
+++ authorization.js
@@ -0,0 +1,21 @@
+const {
+  rule,
+  inputRule,
+  allow,
+  deny,
+  and,
+  chain,
+  or,
+  not,
+} = require('graphql-shield')
+
+module.exports = {
+  rule,
+  inputRule,
+  allow,
+  deny,
+  and,
+  chain,
+  or,
+  not,
+}
