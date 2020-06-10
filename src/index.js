e2e1c26ee9963565eb8647f7b98e1bf3b1941547
jgutix <whan.kharlos@gmail.com>
2020-06-10 11:57:36 -0600

    feat(server): add custom base-model

--- src/index.js
+++ src/index.js
@@ -1,10 +1,10 @@
 const cron = require('node-cron')
 
-const BaseModel = require('@pubsweet/base-model')
 const authentication = require('pubsweet-server/src/authentication')
 const logger = require('@pubsweet/logger')
 
 const app = require('./app')
+const { BaseModel } = require('./baseModel')
 
 const createJWT = authentication.token.create
 
