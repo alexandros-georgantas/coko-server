f6343753c1edb1ba47711d58663e7a43ea22c71e
Andy Nicholson <intothemist@gmail.com>
2021-05-27 16:44:26 +1000

    feat(*): ported chat messages, threads and relatedObjects , tests working

--- config/components.js
+++ config/components.js
@@ -1,1 +1,9 @@
-module.exports = ['./src/models/fake']
+module.exports = 
+	[
+	'@pubsweet/model-user',
+	'@pubsweet/model-team',
+	'./src/models/fake',
+ 	'./src/models/chatMessage',	
+	'./src/models/chatRelatedObject',
+	'./src/models/chatThread',
+	]
