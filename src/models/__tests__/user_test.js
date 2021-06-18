a91bc3c41205353c8fc888f6c345a9b0bfaf0c3d
Andy Nicholson <intothemist@gmail.com>
2021-06-18 17:09:57 +1000

    feat(server): add more user tests adapted from model-user

--- src/models/__tests__/user_test.js
+++ src/models/__tests__/user_test.js
@@ -61,4 +61,76 @@ describe('User', () => {
     expect(stringifiedUser).toHaveProperty('username', user.username)
     expect(stringifiedUser).not.toHaveProperty('passwordHash')
   })
+
+  it('uses custom JSON serialization in an array', async () => {
+    const users = [
+      { username: 'user1', password: 'fooqazwsx1' },
+      { username: 'user2', password: 'fooqazwsx2' },
+      { username: 'user3', password: 'fooqazwsx3' },
+    ]
+
+    await Promise.all(users.map(user => User.query().insert({ ...user })))
+
+    const savedUsers = await User.all()
+
+    const savedUser = savedUsers[2]
+    expect(savedUser).toHaveProperty('username')
+    expect(savedUser).toHaveProperty('passwordHash')
+
+    const stringifiedUsers = JSON.parse(JSON.stringify(savedUsers))
+    const stringifiedUser = stringifiedUsers[2]
+
+    expect(stringifiedUser).toHaveProperty('username', savedUser.username)
+    expect(stringifiedUser).not.toHaveProperty('passwordHash')
+  })
+
+  it('finds a list of users', async () => {
+    const users = [
+      { username: 'user1', password: 'fooqazwsx1' },
+      { username: 'user2', password: 'fooqazwsx2' },
+      { username: 'user3', password: 'fooqazwsx3' },
+    ]
+
+    await Promise.all(users.map(user => User.query().insert({ ...user })))
+
+    const items = await User.findByField('username', 'user1')
+
+    expect(items).toHaveLength(1)
+    expect(items[0]).toBeInstanceOf(User)
+  })
+
+  it('finds a single user by field', async () => {
+    const users = [
+      { username: 'user1', password: 'fooqazwsx1' },
+      { username: 'user2', password: 'fooqazwsx2' },
+      { username: 'user3', password: 'fooqazwsx3' },
+    ]
+
+    await Promise.all(users.map(user => User.query().insert({ ...user })))
+
+    const item = await User.findOneByField('username', 'user2')
+
+    expect(item).toBeInstanceOf(User)
+
+    expect(item).toEqual(
+      expect.objectContaining({
+        username: 'user2',
+        // XXX add in passwordHash comparison
+      }),
+    )
+  })
+
+  it('fails password verification if passwordHash is not present', async () => {
+    const fixtureWithoutPassword = { ...fixtures.user }
+    delete fixtureWithoutPassword.password
+
+    const user = await User.query().insert({ ...fixtureWithoutPassword })
+
+    const validPassword1 = await user.validPassword(undefined)
+    expect(validPassword1).toEqual(false)
+    const validPassword2 = await user.validPassword(null)
+    expect(validPassword2).toEqual(false)
+    const validPassword3 = await user.validPassword('somethingfunky')
+    expect(validPassword3).toEqual(false)
+  })
 })
