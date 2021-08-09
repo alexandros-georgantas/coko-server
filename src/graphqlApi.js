20fac016145814c2657e7a4fd1c7754af003735b
Yannis Barlas <yannisbarlas@gmail.com>
2021-08-09 20:37:10 +0200

    fix(middleware): server host variable should not be needed in prod

--- src/graphqlApi.js
+++ src/graphqlApi.js
@@ -18,15 +18,75 @@ const helpers = require('pubsweet-server/src/helpers/authorization')
 
 const schema = require('./graphqlSchema')
 
-const host = `${config.get('pubsweet-server.host')}${
-  config.get('pubsweet-server.port')
-    ? `:${config.get('pubsweet-server.port')}`
-    : ''
-}`
+const isDevelopment = process.env.NODE_ENV === 'development'
 
 const extraApolloConfig = config.has('pubsweet-server.apollo')
   ? config.get('pubsweet-server.apollo')
   : {}
+
+const apolloServerConfig = {
+  schema,
+  context: ({ req, res }) => ({
+    helpers,
+    connectors,
+    user: req.user,
+    loaders: loaders(),
+
+    req,
+    res,
+  }),
+  formatError: err => {
+    const error = isEmpty(err.originalError) ? err : err.originalError
+
+    logger.error(error.message, { error })
+
+    const isPubsweetDefinedError = Object.values(errors).some(
+      pubsweetError => error instanceof pubsweetError,
+    )
+
+    const isGraphqlDefinedError = [
+      ForbiddenError,
+      UserInputError,
+      AuthenticationError,
+      ApolloError,
+    ].some(graphqlError => error instanceof graphqlError)
+
+    // err is always a GraphQLError which should be passed to the client
+    if (
+      !isEmpty(err.originalError) &&
+      !isPubsweetDefinedError &&
+      !isGraphqlDefinedError
+    )
+      return {
+        name: 'Server Error',
+        message: 'Something went wrong! Please contact your administrator',
+      }
+
+    if (isGraphqlDefinedError) return error
+
+    return {
+      name: error.name || 'GraphQLError',
+      message: error.message,
+      extensions: {
+        code: err.extensions.code,
+      },
+    }
+  },
+  introspection: process.env.NODE_ENV === 'development',
+  ...extraApolloConfig,
+}
+
+if (isDevelopment) {
+  const host = `${config.get('pubsweet-server.host')}${
+    config.get('pubsweet-server.port')
+      ? `:${config.get('pubsweet-server.port')}`
+      : ''
+  }`
+
+  apolloServerConfig.playground = {
+    subscriptionEndpoint: `ws://${host}/subscriptions`,
+  }
+}
 
 const api = app => {
   app.use(
@@ -36,64 +96,7 @@ const api = app => {
     }),
   )
 
-  const server = new ApolloServer({
-    schema,
-    context: ({ req, res }) => ({
-      helpers,
-      connectors,
-      user: req.user,
-      loaders: loaders(),
-
-      req,
-      res,
-    }),
-    formatError: err => {
-      const error = isEmpty(err.originalError) ? err : err.originalError
-
-      logger.error(error.message, { error })
-
-      const isPubsweetDefinedError = Object.values(errors).some(
-        pubsweetError => error instanceof pubsweetError,
-      )
-
-      const isGraphqlDefinedError = [
-        ForbiddenError,
-        UserInputError,
-        AuthenticationError,
-        ApolloError,
-      ].some(graphqlError => error instanceof graphqlError)
-
-      // err is always a GraphQLError which should be passed to the client
-      if (
-        !isEmpty(err.originalError) &&
-        !isPubsweetDefinedError &&
-        !isGraphqlDefinedError
-      )
-        return {
-          name: 'Server Error',
-          message: 'Something went wrong! Please contact your administrator',
-        }
-
-      if (isGraphqlDefinedError) return error
-
-      return {
-        name: error.name || 'GraphQLError',
-        message: error.message,
-        extensions: {
-          code: err.extensions.code,
-        },
-      }
-    },
-    playground:
-      process.env.NODE_ENV === 'production'
-        ? false
-        : {
-            subscriptionEndpoint: `ws://${host}/subscriptions`,
-          },
-    introspection: process.env.NODE_ENV === 'development',
-    ...extraApolloConfig,
-  })
-
+  const server = new ApolloServer(apolloServerConfig)
   server.applyMiddleware({ app })
 }
 
