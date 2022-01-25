622a64cbac8646e98c058f251ee801ca74930d36
Yannis Barlas <yannisbarlas@gmail.com>
2022-01-25 15:34:50 +0200

    fix(server): fix Upload scalar resolver

--- src/graphqlSchema.js
+++ src/graphqlSchema.js
@@ -1,13 +1,64 @@
 const config = require('config')
 const isEmpty = require('lodash/isEmpty')
+const merge = require('lodash/merge')
+
 const { applyMiddleware } = require('graphql-middleware')
 const { shield } = require('graphql-shield')
+const { GraphQLUpload } = require('graphql-upload')
+const { makeExecutableSchema } = require('apollo-server-express')
 
 const logger = require('@pubsweet/logger')
-const schema = require('pubsweet-server/src/graphql/schema')
+const upload = require('pubsweet-server/src/graphql/definitions/upload')
+const tryRequireRelative = require('pubsweet-server/src/helpers/tryRequireRelative')
 
 const emailMiddleware = require('./middleware/email')
 
+// #region BUILD-SCHEMA
+// load base types and resolvers
+const typeDefs = [
+  `type Query, type Mutation, type Subscription`,
+  upload.typeDefs,
+]
+
+const resolvers = merge({}, upload.resolvers, {
+  Upload: GraphQLUpload,
+})
+
+// recursively merge in component types and resolvers
+function getSchemaRecursively(componentName) {
+  const component = tryRequireRelative(componentName)
+
+  if (component.extending) {
+    getSchemaRecursively(component.extending)
+  }
+
+  if (component.typeDefs) {
+    typeDefs.push(component.typeDefs)
+  }
+  if (component.resolvers) {
+    merge(resolvers, component.resolvers)
+  }
+}
+
+if (config.has('pubsweet.components')) {
+  config.get('pubsweet.components').forEach(componentName => {
+    getSchemaRecursively(componentName)
+  })
+}
+
+// merge in app-specific types and resolvers from config
+if (config.has('pubsweet-server.typeDefs')) {
+  typeDefs.push(config.get('pubsweet-server.typeDefs'))
+}
+
+if (config.has('pubsweet-server.resolvers')) {
+  merge(resolvers, config.get('pubsweet-server.resolvers'))
+}
+
+const schema = makeExecutableSchema({ typeDefs, resolvers })
+// #endregion BUILD-SCHEMA
+
+// #region GRAPHQL-MIDDLEWARE
 const baseMessage = 'Coko server =>'
 
 const logRegistration = name =>
@@ -47,6 +98,7 @@ if (!isEmpty(emailConfig)) {
 }
 
 console.log('') // eslint-disable-line no-console
+// #endregion GRAPHQL-MIDDLEWARE
 
 const schemaWithMiddleWare = applyMiddleware(schema, ...middleware)
 
