3f9eccc3d81c8ff81c1acf43c7d2e41f0c3c1205
Yannis Barlas <yannisbarlas@gmail.com>
2022-12-16 14:46:25 +0000

    feat(server): callMicroservice function with authentication

--- src/models/serviceCredential/migrations/1639155624-init-service-credentials.js
+++ src/models/serviceCredential/migrations/1639155624-init-service-credentials.js
@@ -1,23 +1,22 @@
-// /* eslint-disable no-console */
-// const logger = require('@pubsweet/logger')
+const logger = require('@pubsweet/logger')
 
-// exports.up = async knex => {
-//   try {
-//     return knex.schema.createTable('service_credentials', table => {
-//       table.uuid('id').primary()
-//       table
-//         .timestamp('created', { useTz: true })
-//         .notNullable()
-//         .defaultTo(knex.fn.now())
-//       table.timestamp('updated', { useTz: true })
-//       table.text('type').notNullable()
-//       table.text('name').notNullable()
-//       table.text('accessToken').nullable()
-//     })
-//   } catch (e) {
-//     logger.error('Service Credentials: Initial: Migration failed!')
-//     throw new Error(e)
-//   }
-// }
+exports.up = async knex => {
+  try {
+    return knex.schema.createTable('service_credential', table => {
+      table.uuid('id').primary()
+      table
+        .timestamp('created', { useTz: true })
+        .notNullable()
+        .defaultTo(knex.fn.now())
+      table.timestamp('updated', { useTz: true })
+      table.text('type').notNullable()
+      table.text('name').notNullable()
+      table.text('accessToken').nullable()
+    })
+  } catch (e) {
+    logger.error('Service Credentials: Initial: Migration failed!')
+    throw new Error(e)
+  }
+}
 
-// exports.down = async knex => knex.schema.dropTable('service_credentials')
+exports.down = async knex => knex.schema.dropTable('service_credential')
---
+++ src/utils/getAccessToken.js
@@ -0,0 +1,90 @@
+const get = require('lodash/get')
+const config = require('config')
+const axios = require('axios')
+
+const { ServiceCredential } = require('../models')
+
+const services = config.get('services')
+
+const getAccessToken = async (serviceName, renew = false) => {
+  try {
+    if (!services) {
+      throw new Error('services are undefined')
+    }
+
+    const service = get(services, `${serviceName}`)
+
+    if (!service) {
+      throw new Error(`service ${serviceName} configuration is undefined `)
+    }
+
+    const { clientId, clientSecret, port, protocol, host } = service
+    const buff = Buffer.from(`${clientId}:${clientSecret}`, 'utf8')
+    const base64data = buff.toString('base64')
+
+    const serviceURL = `${protocol}://${host}${port ? `:${port}` : ''}`
+
+    const serviceHealthCheck = await axios({
+      method: 'get',
+      url: `${serviceURL}/healthcheck`,
+    })
+
+    const { data: healthCheckData } = serviceHealthCheck
+    const { message } = healthCheckData
+
+    if (message !== 'Coolio') {
+      throw new Error(`service ${serviceName} is down`)
+    }
+
+    const foundServiceCredential = await ServiceCredential.query().findOne({
+      name: serviceName,
+    })
+
+    if (!foundServiceCredential) {
+      const { data } = await axios({
+        method: 'post',
+        url: `${serviceURL}/api/auth`,
+        headers: { authorization: `Basic ${base64data}` },
+      })
+      const { accessToken } = data
+      await ServiceCredential.query().insert({
+        name: serviceName,
+        accessToken,
+      })
+      return accessToken
+    }
+
+    const { accessToken, id } = foundServiceCredential
+
+    if (!accessToken || renew) {
+      const { data } = await axios({
+        method: 'post',
+        url: `${serviceURL}/api/auth`,
+        headers: { authorization: `Basic ${base64data}` },
+      })
+      const { accessToken: freshAccessToken } = data
+      await ServiceCredential.query().patchAndFetchById(id, {
+        accessToken: freshAccessToken,
+      })
+      return freshAccessToken
+    }
+    return accessToken
+  } catch (e) {
+    const foundServiceCredential = await ServiceCredential.query().findOne({
+      name: serviceName,
+    })
+
+    if (foundServiceCredential) {
+      await ServiceCredential.query().patchAndFetchById(
+        foundServiceCredential.id,
+        {
+          accessToken: null,
+        },
+      )
+    }
+
+    throw new Error(e)
+  }
+}
+
+module.exports = getAccessToken
