0e4cb8b1dbd8d26e6388140563bf8c6eccc40f85
Alexandros Georgantas <alexgeorg86@gmail.com>
2023-02-24 16:50:59 +0000

    fix(models): more gentle migrations for users teams identities and team members

--- src/models/identity/migrations/1590000000-identity.js
+++ src/models/identity/migrations/1590000000-identity.js
@@ -1,10 +1,11 @@
-/* eslint-disable no-console */
 const logger = require('@pubsweet/logger')
 
 exports.up = async knex => {
   try {
-    return knex.schema
-      .createTable('identities', table => {
+    const tableExists = await knex.schema.hasTable('identities')
+
+    if (!tableExists) {
+      await knex.schema.createTable('identities', table => {
         table.uuid('id').primary()
         table
           .timestamp('created', { useTz: true })
@@ -26,15 +27,131 @@ exports.up = async knex => {
         table.unique('email', 'unique_email')
         table.unique('verification_token', 'unique_verification_token')
       })
-      .then(() => {
-        knex.schema
-          .raw(
-            ' CREATE UNIQUE INDEX "is_default_idx" ON "identities" (is_default, user_id) WHERE is_default IS true;',
-          )
-          .catch(err => {
-            logger.error(err.stack)
-          })
-      })
+
+      await knex.schema.raw(
+        ' CREATE UNIQUE INDEX "is_default_idx" ON "identities" (is_default, user_id) WHERE is_default IS true;',
+      )
+
+      return true
+    }
+
+    const hasId = await knex.schema.hasColumn('identities', 'id')
+    const hasCreated = await knex.schema.hasColumn('identities', 'created')
+    const hasUpdated = await knex.schema.hasColumn('identities', 'updated')
+    const hasUserId = await knex.schema.hasColumn('identities', 'user_id')
+    const hasProvider = await knex.schema.hasColumn('identities', 'provider')
+
+    const hasOAuthAccessToken = await knex.schema.hasColumn(
+      'identities',
+      'oauth_access_token',
+    )
+
+    const hasOAuthRefreshToken = await knex.schema.hasColumn(
+      'identities',
+      'oauth_refresh_token',
+    )
+
+    const hasIsDefault = await knex.schema.hasColumn('identities', 'is_default')
+    const hasEmail = await knex.schema.hasColumn('identities', 'email')
+
+    const hasIsVerified = await knex.schema.hasColumn(
+      'identities',
+      'is_verified',
+    )
+
+    const hasIsSocial = await knex.schema.hasColumn('identities', 'is_social')
+
+    const hasProfileData = await knex.schema.hasColumn(
+      'identities',
+      'profile_data',
+    )
+
+    const hasVerificationToken = await knex.schema.hasColumn(
+      'identities',
+      'verification_token',
+    )
+
+    const hasVerificationTokenTimestamp = await knex.schema.hasColumn(
+      'identities',
+      'verification_token_timestamp',
+    )
+
+    const hasType = await knex.schema.hasColumn('identities', 'type')
+
+    await knex.schema.alterTable('identities', table => {
+      if (!hasId) {
+        table.dropPrimary('identities_pkey')
+        table.uuid('id').primary()
+      }
+
+      if (!hasCreated) {
+        table.timestamp('created').defaultTo(knex.fn.now())
+      }
+
+      if (!hasUpdated) {
+        table.timestamp('updated').defaultTo(knex.fn.now())
+      }
+
+      if (!hasUserId) {
+        table.dropForeign('user_id')
+        table.uuid('user_id').references('users.id').notNullable()
+      }
+
+      if (!hasProvider) {
+        table.text('provider')
+      }
+
+      if (!hasOAuthAccessToken) {
+        table.text('oauthAccessToken')
+      }
+
+      if (!hasOAuthRefreshToken) {
+        table.text('oauthRefreshToken')
+      }
+
+      if (!hasIsDefault) {
+        table.boolean('is_default').defaultTo(false)
+      }
+
+      if (!hasEmail) {
+        table.text('email').notNullable()
+      }
+
+      if (!hasIsVerified) {
+        table.boolean('is_verified').notNullable().defaultTo(false)
+      }
+
+      if (!hasIsSocial) {
+        table.boolean('is_social')
+      }
+
+      if (!hasProfileData) {
+        table.jsonb('profile_data')
+      }
+
+      if (!hasVerificationToken) {
+        table.text('verification_token')
+      }
+
+      if (!hasVerificationTokenTimestamp) {
+        table.timestamp('verification_token_timestamp', { useTz: true })
+      }
+
+      if (!hasType) {
+        table.text('type').notNullable()
+      }
+
+      table.dropUnique('email', 'unique_email')
+      table.unique('email', 'unique_email')
+      table.dropUnique('verification_token', 'unique_verification_token')
+      table.unique('verification_token', 'unique_verification_token')
+    })
+
+    await knex.schema.raw(
+      ' CREATE UNIQUE INDEX IF NOT EXISTS "is_default_idx" ON "identities" (is_default, user_id) WHERE is_default IS true;',
+    )
+
+    return true
   } catch (e) {
     logger.error('Identity: Initial: Migration failed!')
     throw new Error(e)
--- src/models/user/migrations/1567588786-user.js
+++ src/models/user/migrations/1567588786-user.js
@@ -1,32 +1,144 @@
-/* eslint-disable func-names */
-/* eslint-disable no-console */
-
 const logger = require('@pubsweet/logger')
 
 exports.up = async knex => {
   try {
-    return knex.schema.createTable('users', table => {
-      table.uuid('id').primary()
-      table
-        .timestamp('created', { useTz: true })
-        .notNullable()
-        .defaultTo(knex.fn.now())
-      table.timestamp('updated', { useTz: true })
-      table.string('username')
-      table.text('password_hash')
-      table.text('password_reset_token')
-      table.timestamp('password_reset_timestamp', { useTz: true })
-      table.boolean('agreed_tc').notNullable().defaultTo(false)
-      table.boolean('is_active').notNullable().defaultTo(false)
-      table.text('invitation_token')
-      table.timestamp('invitation_token_timestamp', { useTz: true })
-      table.text('given_names')
-      table.text('surname')
-      table.text('title_pre')
-      table.text('title_post')
-      table.text('type').notNullable()
+    const tableExists = await knex.schema.hasTable('users')
+
+    if (!tableExists) {
+      await knex.schema.createTable('users', table => {
+        table.uuid('id').primary()
+        table
+          .timestamp('created', { useTz: true })
+          .notNullable()
+          .defaultTo(knex.fn.now())
+        table.timestamp('updated', { useTz: true })
+        table.string('username')
+        table.text('password_hash')
+        table.text('password_reset_token')
+        table.timestamp('password_reset_timestamp', { useTz: true })
+        table.boolean('agreed_tc').notNullable().defaultTo(false)
+        table.boolean('is_active').notNullable().defaultTo(false)
+        table.text('invitation_token')
+        table.timestamp('invitation_token_timestamp', { useTz: true })
+        table.text('given_names')
+        table.text('surname')
+        table.text('title_pre')
+        table.text('title_post')
+        table.text('type').notNullable()
+        table.unique('username')
+      })
+      return true
+    }
+
+    const hasId = await knex.schema.hasColumn('users', 'id')
+    const hasCreated = await knex.schema.hasColumn('users', 'created')
+    const hasUpdated = await knex.schema.hasColumn('users', 'updated')
+    const hasUsername = await knex.schema.hasColumn('users', 'username')
+
+    const hasPasswordHash = await knex.schema.hasColumn(
+      'users',
+      'password_hash',
+    )
+
+    const hasPasswordResetToken = await knex.schema.hasColumn(
+      'users',
+      'password_reset_token',
+    )
+
+    const hasPasswordResetTimestamp = await knex.schema.hasColumn(
+      'users',
+      'password_reset_timestamp',
+    )
+
+    const hasAgreedTC = await knex.schema.hasColumn('users', 'agreed_tc')
+    const hasIsActive = await knex.schema.hasColumn('users', 'is_active')
+
+    const hasInvitationToken = await knex.schema.hasColumn(
+      'users',
+      'invitation_token',
+    )
+
+    const hasInvitationTokenTimestamp = await knex.schema.hasColumn(
+      'users',
+      'invitation_token_timestamp',
+    )
+
+    const hasGivenNames = await knex.schema.hasColumn('users', 'given_names')
+    const hasSurname = await knex.schema.hasColumn('users', 'surname')
+    const hasTitlePre = await knex.schema.hasColumn('users', 'title_pre')
+    const hasTitlePost = await knex.schema.hasColumn('users', 'title_post')
+    const hasType = await knex.schema.hasColumn('users', 'type')
+
+    await knex.schema.alterTable('users', table => {
+      if (!hasId) {
+        table.dropPrimary('users_pkey')
+        table.uuid('id').primary()
+      }
+
+      if (!hasCreated) {
+        table.timestamp('created').defaultTo(knex.fn.now())
+      }
+
+      if (!hasUpdated) {
+        table.timestamp('updated').defaultTo(knex.fn.now())
+      }
+
+      if (!hasUsername) {
+        table.string('username')
+      }
+
+      if (!hasPasswordHash) {
+        table.text('password_hash')
+      }
+
+      if (!hasPasswordResetToken) {
+        table.text('password_reset_token')
+      }
+
+      if (!hasPasswordResetTimestamp) {
+        table.timestamp('password_reset_timestamp', { useTz: true })
+      }
+
+      if (!hasAgreedTC) {
+        table.boolean('agreed_tc').notNullable().defaultTo(false)
+      }
+
+      if (!hasIsActive) {
+        table.boolean('is_active').notNullable().defaultTo(false)
+      }
+
+      if (!hasInvitationToken) {
+        table.text('invitation_token')
+      }
+
+      if (!hasInvitationTokenTimestamp) {
+        table.timestamp('invitation_token_timestamp', { useTz: true })
+      }
+
+      if (!hasGivenNames) {
+        table.text('given_names')
+      }
+
+      if (!hasSurname) {
+        table.text('surname')
+      }
+
+      if (!hasTitlePre) {
+        table.text('title_pre')
+      }
+
+      if (!hasTitlePost) {
+        table.text('title_post')
+      }
+
+      if (!hasType) {
+        table.text('type').notNullable()
+      }
+
+      table.dropUnique('username', 'users_username_unique')
       table.unique('username')
     })
+    return true
   } catch (e) {
     logger.error('Users: Initial: Migration failed!')
     throw new Error(e)
