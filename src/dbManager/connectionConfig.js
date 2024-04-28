5589a8ec9055ebc973b22d4d708648a54cf93fc5
Yannis Barlas <yannisbarlas@gmail.com>
2024-04-28 23:40:21 +0200

    feat(db-manager): add POSTGRES_ALLOW_SELF_SIGNED_CERTIFICATES env variable

--- src/dbManager/connectionConfig.js
+++ src/dbManager/connectionConfig.js
@@ -1,7 +1,14 @@
 const config = require('config')
 
+const { isEnvVariableTrue } = require('../utils/env')
+
 const connection =
   process.env.DATABASE_URL ||
   (config['pubsweet-server'] && config['pubsweet-server'].db)
+
+if (isEnvVariableTrue(process.env.POSTGRES_ALLOW_SELF_SIGNED_CERTIFICATES)) {
+  if (!connection.ssl) connection.ssl = {}
+  connection.ssl.rejectUnauthorized = false
+}
 
 module.exports = connection
